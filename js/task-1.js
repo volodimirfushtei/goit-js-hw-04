0d1252508f17ca2ec3878fbd303383cdf7bf789d
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-05-29 03:38:52 +0300

    add task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,14 @@
 function isEnoughCapacity(products, containerSize) {
-  products = {
-    apples: 2,
-    grapes: 4,
-    carrots: 1,
-  };
+  let sumKeys = 0;
+  for (let i in products) {
+    sumKeys += products[i];
+    if (sumKeys <= containerSize) {
+      return "true";
+    } else {
+      return "false";
+    }
+  }
 }
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
